13339e81f8f10983e52d03ae3072535f74635a73
Yohei Sato <sato.yohei@mil-inc.com>
2020-12-02 23:09:34 +0900

    修正
    修正

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import App from './components/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 
 class App extends React.Component {
   constructor(props) {
