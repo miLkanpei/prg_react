c70f760d2f98b4a45000cc04d14d9262c066a519
Yohei Sato <sato.yohei@mil-inc.com>
2020-12-02 22:24:29 +0900

    コミット
    コミット

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Hinoarashi'};
+  }
+
+  // handleClickメソッドを定義
+  handleClick(name){
+    this.setState({name:name});
+  }
+
+  render() {
+    return (
+    	<div className="App-center">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    	  <h1>I choose you! {this.state.name}！</h1>
+    	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
+        <button onClick={() => {this.handleClick('Pikatyuu')}}>
+          Pikatyuu</button>
+
+    	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
+        <button onClick={() => {this.handleClick('Hinoarashi')}}>
+        Hinoarashi</button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
