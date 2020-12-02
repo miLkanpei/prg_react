68b09afedfded8848f1e053bb038b40e24a33110
Yohei Sato <sato.yohei@mil-inc.com>
2020-12-02 23:08:01 +0900

    プロゲート無料版終わり
    プロゲート無料版終わり

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import App from './components/App';
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {name: 'Hinoarashi'};
+    this.state = {count: 0};
   }
 
-  // handleClickメソッドを定義
-  handleClick(name){
+  // ポケモンメソッドを定義
+  pokemon(name){
     this.setState({name:name});
+  }
+
+  // カウントアップメソッドを定義
+  countUp(){
+    this.setState({count:this.state.count + 1});
+  }
+
+  // カウントダウンメソッドを定義
+  countDawn(){
+    this.setState({count:this.state.count - 1});
   }
 
   render() {
     return (
     	<div className="App-center">
+        <h1 className="App-text-color-red">【 MiL.inc React Tutorial 】</h1>
         <img src={logo} className="App-logo" alt="logo" />
-    	  <h1>I choose you! {this.state.name}！</h1>
-    	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
-        <button onClick={() => {this.handleClick('Pikatyuu')}}>
-          Pikatyuu</button>
 
-    	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
-        <button onClick={() => {this.handleClick('Hinoarashi')}}>
-        Hinoarashi</button>
+        <h1>Q1.ポケモンゲットだぜ</h1>
+    	  <h2>I choose you! {this.state.name}！</h2>
+        <button onClick={() => {this.pokemon('Pikatyuu')}}>Pikatyuu</button>
+        <button onClick={() => {this.pokemon('Hinoarashi')}}>Hinoarashi</button>
 
+        <h1>Q2.カウントアップ・ダウン</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => {this.countUp()}}>+</button>
+        <button onClick={() => {this.countDawn()}}>-</button>
       </div>
     );
   }
